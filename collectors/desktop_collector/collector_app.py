e936f00e1edd0efd7b74561b653dd1967d91b1c4
Badr Eddine BELGHARBI <badreddine.belgharbi@gmail.com>
2026-07-02 09:07:07 +0200

    Bundle macOS osquery from Homebrew

--- collectors/desktop_collector/collector_app.py
+++ collectors/desktop_collector/collector_app.py
@@ -51,7 +51,7 @@ else:
 
 
 DEFAULT_API_URL = "https://oletfrcaptvardmdwacy.supabase.co/functions/v1/inventory-api"
-COLLECTOR_VERSION = "0.1.45"
+COLLECTOR_VERSION = "0.1.46"
 COLLECTOR_BUILD_CHANNEL = "github-release"
 COLLECTOR_RELEASES_URL = "https://badr-spacefoot.github.io/pc_inventory_2.0/collector-releases.json"
 DRAFT_PATH = Path.home() / ".spacefoot_it_collector.json"
